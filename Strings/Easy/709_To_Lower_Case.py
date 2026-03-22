935d62f326ebd5bbe46fbe203b6c286ac120a2bd
Olga-2308 <olgaokorokova1103@gmail.com>
2026-03-22 16:28:18 +0700

    Solve task #709 To Lower Case

---
+++ Strings/Easy/709_To_Lower_Case.py
@@ -0,0 +1,5 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        # using the lower() method we convert all characters in the string to lowercase
+        result = s.lower()
+        return result
