76853448ce30c1419398c13b5eebc8864e943b36
Olga-2308 <olgaokorokova1103@gmail.com>
2026-05-26 22:39:45 +0700

    Solve task #2903 Find Indices With Index and Value Difference I

--- Array/Easy/2903_Find_Indices_With_Index_and_Value_Difference_I.py
+++ Array/Easy/2903_Find_Indices_With_Index_and_Value_Difference_I.py
@@ -1,7 +1,8 @@
 class Solution:
     def findIndices(self, nums: List[int], indexDifference: int, valueDifference: int) -> List[int]:
         '''
-        Using a nested loop, we find pairs of numbers that match the conditions of the problem
+        Using a nested loop, we find pairs of numbers 
+        that match the conditions of the problem
         '''
 
         for i in range(len(nums)):
